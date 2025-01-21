50f9414731ffe65c154390813e54cea03b5e3ac0
Yoav Nahshon <yoav.nahshon@iwm.frasunhofer.de>
2025-01-21 06:20:39 -0500

    Adjust message priority level: Add SKIP_ENV_CHECK

--- shieldapi/__init__.py
+++ shieldapi/__init__.py
@@ -9,8 +9,16 @@ logger = logging.getLogger("shieldapi")
 def check_mandatory_env_vars():
     """
     Checks if all mandatory environment variables are set.
+    Skips the check if SKIP_ENV_CHECK is set.
     Raises an EnvironmentError if any are missing.
     """
+    # Allow skipping the check if SKIP_ENV_CHECK is set
+    if os.environ.get("SKIP_ENV_CHECK"):
+        logger.info(
+            "Skipping mandatory environment variable check due to SKIP_ENV_CHECK."
+        )
+        return
+
     mandatory_vars = [
         "KEYCLOAK_HOST",
         "KEYCLOAK_REALM_NAME",
@@ -21,7 +29,7 @@ def check_mandatory_env_vars():
     missing_vars = [var for var in mandatory_vars if not os.environ.get(var)]
 
     if missing_vars:
-        logger.critical(
+        logger.warning(
             "Missing mandatory environment variables: %s", ", ".join(missing_vars)
         )
 
